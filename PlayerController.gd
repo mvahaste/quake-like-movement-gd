cfc0dc8f54908b93a4508fda662383f819dbba5e
M <48407846+mvahaste@users.noreply.github.com>
2022-12-26 12:20:55 +0200

    *

--- PlayerController.gd
+++ PlayerController.gd
@@ -4,54 +4,60 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
-const MAX_SPEED = 128
-const MAX_ACCEL = 10 * MAX_SPEED
-const MOUSE_SENSITIVITY = 0.001
-
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+var last_floor = true
+var wishdir = Vector3()
+var prev_wishdir = Vector3()
+var direction = Vector3()
+
 
 func _ready():
-	# Lock mouse
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 
 func _input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(-event.relative.x) * 0.05)
+		$Head.rotate_x(deg_to_rad(-event.relative.y) * 0.05)
+		$Head.rotation.x = clamp($Head.rotation.x, deg_to_rad(-89), deg_to_rad(89))
+
 	if Input.is_action_just_pressed("escape"):
 		get_tree().quit()
-	
-	# Move mouse
-	if event is InputEventMouseMotion:
-		rotate_y(-event.relative.x * MOUSE_SENSITIVITY)
-		$Head.rotate_x(-event.relative.y * MOUSE_SENSITIVITY)
 
 
 func _process(delta):
-	# TODO: Add the gravity.
-	# if not is_on_floor():
-	# 	velocity.y -= gravity * delta
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
 
-	# TODO: Handle Jump.
-	# if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-	# 	velocity.y = JUMP_VELOCITY
+	# Handle Jump.
+	if Input.is_action_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		prev_wishdir = wishdir
 
 	# Get the input direction and handle the movement/deceleration.
-	var wishdir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
-	var direction = (transform.basis * Vector3(wishdir.x, 0, wishdir.y)).normalized()
+	var input = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	wishdir = Vector3(input.x, 0, input.y)
+	
+	print(wishdir)
 
-	# var current_speed = velocity * Vector3(wishdir.x, 0, wishdir.y)
-
-	# print(Vector3(wishdir.x, 1.25, wishdir.y))
-
-	$WishdirRaycast.target_position = lerp($WishdirRaycast.target_position, Vector3(wishdir.x, 0, wishdir.y) * 3, 0.25)
-	$Head/Label.text = "vel: %.3f" % velocity.length()
+	direction = lerp(direction, (transform.basis * wishdir).normalized(), 0.1)
 
 	if direction:
-		velocity.x = direction.x * MAX_ACCEL * delta
-		velocity.z = direction.z * MAX_ACCEL * delta
+		# velocity.x = direction.x * SPEED
+		velocity.x = lerp(velocity.x, direction.x * SPEED, 0.1)
+		# velocity.z = direction.z * SPEED
+		velocity.z = lerp(velocity.z, direction.z * SPEED, 0.1)
 	else:
-		velocity.x = move_toward(velocity.x, 0, MAX_ACCEL) * delta
-		velocity.z = move_toward(velocity.z, 0, MAX_ACCEL) * delta
+		if is_on_floor() and last_floor:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			velocity.z = move_toward(velocity.z, 0, SPEED)
+	
+	last_floor = is_on_floor()
+	
+	$Head/Label.text = "vel: %.3f" % Vector2(velocity.x, velocity.z).length()
+	$WishdirRaycast.target_position = wishdir * 10
 
 	move_and_slide()
